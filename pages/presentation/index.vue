60e30d3b04be0902c731bcc60d3aaf00e2d02f2e
JoanLeblanc <joan.leblanc@orange.fr>
2021-06-09 13:14:25 +0200

    prez

--- pages/presentation/index.vue
+++ pages/presentation/index.vue
@@ -1,9 +1,9 @@
 <template>
   <main>
     <section v-if="posts" class="w-full max-w-5xl mx-auto">
-      <picture>
+      <div>
         <img src='/assets/images/TRIO_PEX.jpg' alt="Photo du groupe Trio Liny">
-      </picture>
+      </div>
       <h1 class="title">Trio liny</h1>
     </section>
   </main>
