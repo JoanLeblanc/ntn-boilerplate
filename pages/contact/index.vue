cf42f98b9154d1ac9f8e3e29a316a8bc19977ca0
JoanLeblanc <joan.leblanc@orange.fr>
2021-06-09 12:46:07 +0200

    tamplate

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -1,1 +1,7 @@
-<h1> Formulaire de contact</h1>
+<template>
+  <main>
+    <section class="self-center flex flex-col flex-1 items-center justify-center">
+      <h1 class="title text-center">Form Contact</h1>
+    </section>
+  </main>
+</template>
